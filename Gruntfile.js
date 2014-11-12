6ff8181c212df702826812d82fa0e6d70b0e4322
Harry Garrood <harry@garrood.me>
2014-11-12 21:45:22 +0000

    fix build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,48 +1,53 @@
 module.exports = function(grunt) {
   "use strict";
 
-  var commonFiles = [
-    "src/Common/**/*.purs",
+  var sourceFiles = [
+    "src/**/*.purs",
     "bower_components/**/src/**/*.purs",
     "vendor/**/src/**/*.purs"
   ];
-  var serverFiles = commonFiles.concat("src/Server/*.purs");
-  var clientFiles = commonFiles.concat("src/Client/*.purs");
-  var allFiles = commonFiles.concat("src/Server/*.purs", "src/Client/*.purs");
 
   grunt.initConfig({
     dotPsci: {
-      src: allFiles
+      src: sourceFiles
     },
 
     pscMake: {
-      server: {
-        options: {
-          main: "Server",
-          modules: ["Server"]
-        },
-        src: serverFiles,
-        //dest: "bin/server.js",
-      },
-      client: {
-        options: {
-          main: "Client",
-          modules: ["Client"]
-        },
-        src: clientFiles,
-        //dest: "js/client.js",
+      all: {
+        src: sourceFiles
       }
     },
 
-    execute: {
-      server: {
-        src: ["bin/server.js"]
+    copy: [
+      {
+        expand: true,
+        cwd: "output",
+        src: ["**"],
+        dest: "tmp/node_modules/"
+      },
+      {
+        src: ["js/client.js"],
+        dest: "tmp/client.js"
+      },
+      {
+        src: ["js/server.js"],
+        dest: "tmp/server.js"
       }
-    }
+    ],
+
+    browserify: {
+      all: {
+        src: ["tmp/client.js"],
+        dest: "app/js/game.js"
+      }
+    },
   });
 
   grunt.loadNpmTasks("grunt-purescript");
-  grunt.registerTask("make", ["pscMake:client", "pscMake:server", "dotPsci"]);
+  grunt.loadNpmTasks("grunt-browserify");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+
+  grunt.registerTask("make", ["pscMake", "copy", "dotPsci", "browserify"]);
   grunt.registerTask("run", ["make", "execute:server"]);
   grunt.registerTask("default", ["make"]);
 };
