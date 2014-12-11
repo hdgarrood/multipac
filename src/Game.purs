41395b060d25b6c0e93086151bfab2c21db77f25
Harry Garrood <harry@garrood.me>
2014-12-11 19:03:39 +0000

    shorter rampages

--- src/Game.purs
+++ src/Game.purs
@@ -20,7 +20,7 @@ import Utils
 
 
 minPlayers = 2
-rampageLength = 240
+rampageLength = 180
 
 -- minimum squared distance that a player needs to be from another player in
 -- order to eat them, in blocks.
